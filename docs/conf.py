6ec9a893ce066c061245e7a5c1825661e1cce599
LiuXingyu <ucapiuw@ucl.ac.uk>
2025-11-09 22:32:17 +0000

    xx

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ release = '1.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.intersphinx'
+    'sphinx.ext.intersphinx',
     'sphinx.ext.autodoc',
 ]
 
